47f976784e78213e7451860ea7bf68d21d21b7a7
Pablo Jalon <pjalonf@cofares.es>
2025-03-25 09:42:22 +0100

    primer commit

---
+++ src/main/java/org/example/Ejemplo.js
@@ -0,0 +1,5 @@
+let num1 = parseInt(prompt("Teclea un numero entero"));
+let num2 = parseInt(prompt("Teclea un numero entero"));
+let num3 = parseInt(prompt("Teclea un numero entero"));
+let media = (num1 + num2 + num3)/3;
+alert("La media de los números es "+media);
